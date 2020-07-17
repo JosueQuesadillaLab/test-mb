a15a1281dc1519b1f124f8bdebe79f0646be85cc
Josué Casasola <j.casasola.lopez@gmail.com>
2020-07-17 02:10:47 -0500

    fix files

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('test-mb app is running!');
+    expect(page.getTitleText()).toEqual('music-battle app is running!');
   });
 
   afterEach(async () => {
